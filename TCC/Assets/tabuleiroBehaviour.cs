29cf4894cb3a89f22a3aeb6d1ae8aabd9a8da3bd
Gabriel Dias <gabrieldias2032@gmail.com>
2019-12-13 10:17:13 -0300

    corrida maluca

--- TCC/Assets/tabuleiroBehaviour.cs
+++ TCC/Assets/tabuleiroBehaviour.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class tabuleiroBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    RaycastHit2D mouse;
+    GameObject clone;
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouse = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+            if (mouse.collider != null && mouse.collider.gameObject.tag == "Pino")
+            {
+                clone = mouse.collider.gameObject;
+                clone.GetComponent<PinoBehaviour>().Drag = true;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(clone != null && clone.GetComponent<PinoBehaviour>().Drag)
+            {
+                clone.GetComponent<PinoBehaviour>().Drag = false;
+            }
+        }
     }
 }
